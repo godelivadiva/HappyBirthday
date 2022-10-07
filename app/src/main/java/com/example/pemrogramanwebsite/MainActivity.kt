4b49850cd34be3a58b63c15f34ecb11edcc40115
godelivdiva <godeliva.divayu@students.utdi.ac.id>
2022-10-07 07:34:41 +0700

    "[ADD] program dictionary use recyclerview and intent"

--- app/src/main/java/com/example/pemrogramanwebsite/MainActivity.kt
+++ app/src/main/java/com/example/pemrogramanwebsite/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.pemrogramanwebsite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnList : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnList = findViewById(R.id.btn_list)
+        btnList.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when(v.id) {
+                R.id.btn_list -> run {
+                    val intentListActivity = Intent(this@MainActivity, WordsActivity::class.java)
+                    startActivity(intentListActivity)
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/pemrogramanwebsite/WordsDetailActivity.kt
@@ -0,0 +1,42 @@
+package com.example.pemrogramanwebsite
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.pemrogramanwebsite.databinding.ActivityWordsDetailBinding
+
+
+class WordsDetailActivity : AppCompatActivity() {
+    // Mendeklarasikan kata "letter" dengan variable LETTER
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Retrieve a binding object that allows you to refer to views by id name
+        // Names are converted from snake case to camel case.
+        // For example, a View with the id word_one is referenced as binding.wordOne
+        val binding = ActivityWordsDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Mendapatkan LETTER yang di deklarasikan pada intent, yaitu key "letter"
+        // intent.extras.getString returns String? (String or null)
+        // so toString() guarantees that the value will be a String
+        val letterId = intent?.extras?.getString(LETTER).toString()
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = WordsAdapter(letterId, this)
+
+        // Adds a [DividerItemDecoration] between items
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        )
+
+        title = getString(R.string.detail_prefix) + " " + letterId
+    }
+}
