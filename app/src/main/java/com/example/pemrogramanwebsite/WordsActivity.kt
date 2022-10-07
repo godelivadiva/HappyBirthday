1074d016925a60f32578cb7f6135ce3ce8d9c629
godelivdiva <godeliva.divayu@students.utdi.ac.id>
2022-10-07 19:18:35 +0700

    "[IMP] Add another documentation for menu"

--- app/src/main/java/com/example/pemrogramanwebsite/WordsActivity.kt
+++ app/src/main/java/com/example/pemrogramanwebsite/WordsActivity.kt
@@ -12,14 +12,14 @@ import com.example.pemrogramanwebsite.databinding.ActivityWordsBinding
 
 class WordsActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
-    // set the linear layout first
+    // set the linear layout first with true (so the icon will be linear at first)
     private var isLinearLayoutManager = true
    // change the layout when clicked the item menu
     private fun chooseLayout() {
         if (isLinearLayoutManager) {
             recyclerView.layoutManager = LinearLayoutManager(this)
         } else {
-            recyclerView.layoutManager = GridLayoutManager(this, 4)
+            recyclerView.layoutManager = GridLayoutManager(this, 4) // set in 4 rows
         }
         recyclerView.adapter = LetterAdapter()
     }
@@ -27,16 +27,21 @@ class WordsActivity : AppCompatActivity() {
     // set the icon for the menu
     private fun setIcon(menuItem: MenuItem?) {
         if (menuItem == null)
-            return
+            return // nothing happened if there's no changing the menu
         menuItem.icon =
+            // set the icon of the menu
             if (isLinearLayoutManager)
+                // change to ic_linear_layout
                 ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+            // change to ic_grid_layout
             else ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // get the menu layout
         menuInflater.inflate(R.menu.layout_menu, menu)
 
+        // call the id of the menu button
         val layoutButton = menu?.findItem(R.id.action_switch_layout)
         // Calls code to set the icon based on the LinearLayoutManager of the RecyclerView
         setIcon(layoutButton)
@@ -55,6 +60,7 @@ class WordsActivity : AppCompatActivity() {
 
                 return true
             }
+            // override this function if there's no itemId (no clicking the button)
             else -> super.onOptionsItemSelected(item)
         }
     }
